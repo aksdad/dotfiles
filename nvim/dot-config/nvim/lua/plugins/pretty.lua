9f6dc85796bbc0028ad0fb0bc8af14aa2f24f61d
Akshay Dadhwal <akshay@aksdad.com>
2025-10-03 19:49:08 -0700

    some updates; add aerospace

--- nvim/dot-config/nvim/lua/plugins/pretty.lua
+++ nvim/dot-config/nvim/lua/plugins/pretty.lua
@@ -31,7 +31,7 @@ return {
     config = function()
       require("rose-pine").setup({
         styles = {
-          transparency = true,
+          -- transparency = true,
         }
       })
       vim.cmd("colorscheme rose-pine")
--- wezterm/dot-wezterm.lua
+++ wezterm/dot-wezterm.lua
@@ -17,8 +17,8 @@ local keys = {
 
 local config = {
     color_scheme = "rose-pine-moon", -- tokyonight_moon
-    window_background_opacity = 0.8,
-    macos_window_background_blur = 20,
+    window_background_opacity = 1,
+    macos_window_background_blur = 40,
     hide_tab_bar_if_only_one_tab = true,
     window_decorations = "RESIZE",
     keys = keys,
